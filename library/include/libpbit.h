c8bcd285bbfab513de2aef4ec48f368f435bdf3f
Sergio Paracuellos <sergio.paracuellos@gmail.com>
2018-11-10 09:46:34 +0100

    library: the general and unique include for client code

---
+++ library/include/libpbit.h
@@ -0,0 +1,21 @@
+/**
+ * @file libpbit.h
+ *
+ * @brief this is the only header file that shall be
+ * included in test code. This is the correct file you
+ * shall include in order to use the pbit framework.
+ *
+ * @author: Sergio Paracuellos <sergio.paracuellos@gmail.com> 
+ */
+
+#ifndef LIB_PBIT_H_
+#define LIB_PBIT_H_
+
+#include <pbit.h>
+#include <pbit_test.h>
+#include <pbit_result.h>
+#include <pbit_runner.h>
+#include <pbit_helpers.h>
+#include <buffer_pbit_io.h>
+
+#endif /* LIB_PBIT_H_ */
